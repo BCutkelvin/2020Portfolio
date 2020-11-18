c8a082eb91d33ce90b21701b6552ea27ac2d7f0f
MrBCut <b.cutkelv@users.noreply.github.com>
2020-11-18 11:28:40 -0800

    adding javascript for homepage

---
+++ welcomePage.js
@@ -0,0 +1,82 @@
+/*
+    Bryan Cutkelvin
+    2020
+      -this markup is using Bootstrap for educational purposes.
+*/
+
+//create variables for welcome header text
+var welcomeHeaderString = 'bryancutkelvin.com Portfolio';
+
+//create variables for feature sections
+var videoGamesSectionString = 'Latest Video Game 🎮 News: ';
+var playstationArticlesString = 'This Week in Playstation...';
+var electronicartsArticlesString = 'This Week in electronic Arts... ';
+var perfectworldArticlesString = 'This Week in Perfect World... ';
+var videogamenewsArticlesString = 'Other Video Game News... ';
+
+var technologySectionString = 'Latest Technology 💻 News: ';
+var amazonwebservicesArticlesString = 'This Week in Amazon Web Services... ';
+var securityArticlesString = 'This Week in Security... ';
+var networkingArticlesString = 'This Week in Networking... ';
+var technologynewsArticlesString = 'Other Technology News... ';
+
+var porscheSectionString = 'Latest Porsche 🏁 News: ';
+var porscheArticlesString = 'This Week in Porsche... ';
+var sportscarnewsArticlesString = 'Other Sports Car News...';
+
+var foodSectionString = 'Latest Smart Kitchen 👨🏼‍🍳 News: ';
+var yummlyArticlesString = 'This Week in Yummly...';
+var smartkitchenArticlesString = 'Other Smart Kitchen News...';
+
+
+var el_videoGamesSection = document.getElementById('videoGamesSection');
+el_videoGamesSection.textContent = videoGamesSectionString;
+
+
+var el_playstationArticles = document.getElementById('playstationarticles');
+el_playstationArticles.textContent = playstationArticlesString;
+
+var el_electronicartsArticles = document.getElementById('electronicartsarticles');
+el_electronicartsArticles.textContent = electronicartsArticlesString;
+
+var el_perfectworldArticles = document.getElementById('perfectworldarticles');
+el_perfectworldArticles.textContent = perfectworldArticlesString;
+
+var el_videogamenewsArticles = document.getElementById('videogamenewsarticles');
+el_videogamenewsArticles.textContent = videogamenewsArticlesString;
+
+
+var el_technologySection = document.getElementById('techncologySection');
+el_technologySection.textContent = technologySectionString;
+
+var el_amazonwebservicesArticles = document.getElementById('amazonwebservicesarticles');
+el_amazonwebservicesArticles.textContent = amazonwebservicesArticlesString;
+
+var el_securityArticles = document.getElementById('securityarticles');
+el_securityArticles.textContent = securityArticlesString;
+
+var el_networkingArticles = document.getElementById('networkingarticles');
+el_networkingArticles.textContent = networkingArticlesString;
+
+var el_technologynewsArticles = document.getElementById('technologynewsarticles');
+el_technologynewsArticles.textContent = technologynewsArticlesString;
+
+
+var el_porscheSection = document.getElementById('porschesSection');
+el_porscheSection.textContent = porscheSectionString;
+
+var el_porscheArticles = document.getElementById('porschearticles');
+el_porscheArticles.textContent = porscheArticlesString;
+
+var el_sportscarnewsArticles = document.getElementById('sportcarnewsarticles');
+el_sportscarnewsArticles.textContent = sportscarnewsArticlesString;
+
+
+var el_foodSection = document.getElementById('foodSection');
+el_foodSection.textContent = foodSectionString;
+
+var el_yummlyarticles = document.getElementById('yummlyarticles');
+el_yummlyarticles.textContent = yummlyArticlesString;
+
+var el_smartkitchenarticles = document.getElementById('smartkitchenarticles');
+el_smartkitchenarticles.textContent = smartkitchenArticlesString;
